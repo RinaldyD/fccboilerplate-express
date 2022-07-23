3aabad2c21c40756e25dbb53fae1d6d43cf9caa1
RinaldyD <darmawan.rinaldy@gmail.com>
2022-07-24 01:09:20 +0700

    add and adjust env json response

--- myApp.js
+++ myApp.js
@@ -1,10 +1,20 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-    res.json({
-        message: "Hello json"
-    });
+    let jsonResponse = { "message": "Hello json" };
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        jsonResponse.message = jsonResponse.message.toUpperCase();
+    }
+    res.json(jsonResponse);
 });
 
 
